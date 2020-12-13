681e01cb17119e9d03d7ba2ba2bc22beb460b729
Rudraditya2004 <73527768+Rudraditya2004@users.noreply.github.com>
2020-12-13 13:45:54 +0530

    Update iris-flower-classification.py

--- iris-flower-classification.py
+++ iris-flower-classification.py
@@ -1,28 +1,84 @@
-# import modules 
-from sklearn.datasets import load_iris  
+# importing modules
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier 
-import numpy as np 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import load_iris
 
-# setting the source
-dataset = load_iris() 
+"""
+0-> setosa
+1-> versicolor
+2-> virginica 
+"""
 
 # training the model
-X_train, X_test, y_train, y_test = train_test_split(dataset.data,dataset.target,test_size=0.3,random_state=0) 
-model = KNeighborsClassifier(n_neighbors=1) 
-model.fit(X_train,y_train) 
+dataset = load_iris()
+main_data = pd.DataFrame(data=dataset.data,columns=["Sepal Length","Sepal Width","Petal Length","Petal Width"])
+main_data["Target"] = dataset["target"]
 
-# asking the user inputs 
-sepal_length = float(input('Enter the sepal length (cm) : ')) 
-sepal_width = float(input('Enter the sepal width (cm) : ')) 
-petal_length = float(input('Enter the petal length (cm) : ')) 
-petal_width = float(input('Enter the petal width (cm) : ')) 
+X_train,X_test,y_train,y_test = train_test_split(dataset["data"],dataset["target"],random_state=0)
+model = KNeighborsClassifier(n_neighbors=1)
+model.fit(X_train,y_train)
+# asking the sepal length, sepal width, petal length, and petal width
+Sepal_length = float(input("Enter the sepal length (cm) : "))
+Sepal_width = float(input("Enter the sepal width (cm) : "))
+Petal_length = float(input("Enter the petal length (cm) : "))
+Petal_width = float(input("Enter the petal width (cm) : "))
+X_new = np.array([[Sepal_length,Sepal_width,Petal_length,Petal_width]])
+prediction = model.predict(X_new)
+# printing the predicted species and the accuracy
+accuracy = model.score(X_test,y_test,sample_weight=None)*100
+print("Predicted Species : {} ({:.2f}%)".format(dataset.target_names[prediction],accuracy))
 
-# creating an array of the inputs 
-New_data = np.array([[sepal_length,sepal_width,petal_length,petal_width]]) 
+# description of the data 
+print()
+print(main_data.describe())
 
-# prediction of the name of the flower 
-prediction = model.predict(New_data) 
+# plotting the sepal length vs sepal width graph
+sepal_length = main_data["Sepal Length"]
+sepal_width = main_data["Sepal Width"]
+plt.scatter(x=sepal_length[range(0,50)],y=sepal_width[range(0,50)],label="Setosa",alpha=0.6)
+plt.scatter(x=sepal_length[range(50,100)],y=sepal_width[range(50,100)],label="Setosa",alpha=0.6)
+plt.scatter(x=sepal_length[range(100,150)],y=sepal_width[range(100,150)],label="Virginica",alpha=0.6)
+plt.title("Sepal Length V/s Sepal Width")
+plt.xlabel("Sepal Length")
+plt.ylabel("Sepal Width")
+plt.legend()
+plt.show()
 
-# printing output
-print('Predicted Target name : {}'.format(dataset.target_names[prediction]))
+# plotting the petal length vs petal width graph
+petal_length = main_data["Petal Length"]
+petal_width = main_data["Petal Width"]
+plt.scatter(x=petal_length[range(0,50)],y=petal_width[range(0,50)],label="Setosa",alpha=0.6)
+plt.scatter(x=petal_length[range(50,100)],y=petal_width[range(50,100)],label="Setosa",alpha=0.6)
+plt.scatter(x=petal_length[range(100,150)],y=petal_width[range(100,150)],label="Virginica",alpha=0.6)
+plt.title("Petal Length V/s Petal Width")
+plt.xlabel("Petal Length")
+plt.ylabel("Petal Width")
+plt.legend()
+plt.show()
+
+# plotting the sepal length vs petal length graph
+sepal_length = main_data["Sepal Length"]
+petal_length = main_data["Petal Length"]
+plt.scatter(x=sepal_length[range(0,50)],y=petal_length[range(0,50)],label="Setosa",alpha=0.6)
+plt.scatter(x=sepal_length[range(50,100)],y=petal_length[range(50,100)],label="Setosa",alpha=0.6)
+plt.scatter(x=sepal_length[range(100,150)],y=petal_length[range(100,150)],label="Virginica",alpha=0.6)
+plt.title("Sepal Length V/s Petal Length")
+plt.xlabel("Sepal Length")
+plt.ylabel("Petal Length")
+plt.legend()
+plt.show()
+
+# plotting the sepal width vs petal width graph
+sepal_width = main_data["Sepal Width"]
+petal_width = main_data["Petal Width"]
+plt.scatter(x=sepal_width[range(0,50)],y=petal_width[range(0,50)],label="Setosa",alpha=0.6)
+plt.scatter(x=sepal_width[range(50,100)],y=petal_width[range(50,100)],label="Setosa",alpha=0.6)
+plt.scatter(x=sepal_width[range(100,150)],y=petal_width[range(100,150)],label="Virginica",alpha=0.6)
+plt.title("Sepal Width V/s Petal Width")
+plt.xlabel("Sepal Width")
+plt.ylabel("Petal Width")
+plt.legend()
+plt.show()
